5ecc305112b460f418d04bb023a4876ab2f5b75b
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-19 15:59:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,11 @@
 provider "aws" {
         region="us-east-1"
 }
-resource "aws_instance" "instance-1" {
-        ami="ami-0ed9277fb7eb570c9"
-        instance_type="t2.micro"
+
+resource "aws_instance" "My-ec2" {
+        instance_type=var.instance_type
+        ami=var.ec2_ami
+        tags ={
+                Name=var.instance_name
+        }
 }
