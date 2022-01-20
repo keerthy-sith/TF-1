684210ceef0aa8b97aa6780dae41b3bc08f26359
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-20 16:01:40 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,6 +1,9 @@
-variable "instance_type" {
+variable ec2_ami{
+default="ami-08e4e35cccc6189f4"
 }
-variable "ec2_ami" {
+variable instance_type{
+default="t2.micro"
 }
-variable "instance_name"{
+variable instance_name{
+default="My-Ec2"
 }
