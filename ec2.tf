6b913e19f9e4fa01c9663633a46373b91d9170e0
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-21 16:24:37 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,11 @@ provider "aws" {
         region="us-east-1"
 }
 
-resource "aws_instance" "My-ec2" {
-        instance_type=var.instance_type
-        ami=var.ec2_ami
-        tags ={
-                Name=var.instance_name
+resource "aws_instance" "Workspace-vm" {
+        ami=lookup(var.ec2_ami,terraform.workspace)
+        instance_type=lookup(var.ec2_instance_type,terraform.workspace)
+        tags={
+                Name=lookup(var.instance_name,terraform.workspace)
         }
 }
+
