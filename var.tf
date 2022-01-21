e541a1485b3bb52e77e37425717156fd017f91af
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-21 16:23:39 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,9 +1,21 @@
-variable ec2_ami{
-default="ami-08e4e35cccc6189f4"
+variable "ec2_instance_type" {
+        type=map
+        default={
+                default="t2.micro"
+                Dev="t2.nano"
+        }
 }
-variable instance_type{
-default="t2.micro"
+variable "ec2_ami" {
+        type=map
+        default={
+                default="ami-08e4e35cccc6189f4"
+                Dev="ami-0d43d465e2051057f"
+        }
 }
-variable instance_name{
-default="My-Ec2"
+variable "instance_name" {
+        type=map
+        default={
+                default="linux"
+                Dev="windows"
+        }
 }
