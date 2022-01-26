101b60ea950af464529abf82049f9882c94bb3f0
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-26 15:26:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "Workspace-vm" {
         ami=lookup(var.ec2_ami,terraform.workspace)
         instance_type=lookup(var.ec2_instance_type,terraform.workspace)
+        key_name="Jenkins"
         tags={
                 Name=lookup(var.instance_name,terraform.workspace)
         }
